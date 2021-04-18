fcc5cb7ee65b10a36d11e996a0f615c10b62bd48
phamquyetthang <43201625+phamquyetthang@users.noreply.github.com>
2021-04-18 16:35:26 +0700

    screen shot

---
+++ src/App/Home.js
@@ -0,0 +1,37 @@
+ /*global chrome*/
+import React, { useEffect, useState } from "react";
+import photoIcon from "./icons/photo.svg";
+import screenshotIcon from "./icons/screenshot.svg";
+const HomeScreen = () => {
+  const [state, setState] = useState()
+  useEffect(() => {
+    chrome.runtime.onMessage.addListener((req, sender, res) => {
+      setState(req)
+    });
+
+  }, [])
+  const takeScreenShot = () => {
+    chrome.runtime.sendMessage(chrome.runtime.id, {
+      from: "popup",
+      subject: "Capture",
+    });
+  }
+  return (
+    <div className="cs-home">
+      <div className="cs-home-cap">
+        <button onClick={takeScreenShot}>
+          <img src={screenshotIcon} alt="icon" />
+        </button>
+      </div>
+      {JSON.stringify(state)}
+      <hr />
+      <div className="cs-home-up">
+        <button>
+          <img src={photoIcon} alt="icon" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default HomeScreen;
---
+++ src/App/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HomeScreen from "./Home";
+
+const Main = () => {
+  return (
+    <div className="cs-app">
+      <HomeScreen />
+    </div>
+  );
+};
+
+export default Main;
